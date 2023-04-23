6c3080eeacdc414de853371bb3802cb0b9022187
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2023-04-22 21:28:41 -0300

    fix typo

--- src/home-tab.js
+++ src/home-tab.js
@@ -2,7 +2,7 @@ const h1 = document.createElement("h1");
 h1.textContent = "Welcome to the SofTaurant";
 const p1 = document.createElement("p");
 p1.textContent =
-  "Everyone's digital restaurant! it's food is delicious, it's beverages are out of this world and the location is the best. You can come alone, with friends or family, everyone's invited. Our waiters have been built to serve up to the speed of your internet connection. So if you POST your order you'll GET your 200!";
+  "Everyone's digital restaurant! its food is delicious, its beverages are out of this world and the location is the best. You can come alone, with friends or family, everyone's invited. Our waiters have been built to serve up to the speed of your internet connection. So if you POST your order you'll GET your 200!";
 const p2 = document.createElement("p");
 p2.textContent =
   "Bye for now, if you need more info head to the menu or contact tabs :)";
